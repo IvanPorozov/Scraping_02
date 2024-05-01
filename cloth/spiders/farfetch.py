cd1763c1d1b189cd6178e1ad6c9f450b43bc8108
Porozov Ivan <porozovivan090@gmail.com>
2024-05-01 19:32:02 +0300

    002

--- cloth/spiders/farfetch.py
+++ cloth/spiders/farfetch.py
@@ -1,12 +1,23 @@
+import xml.etree.ElementTree as ET
 import scrapy
 from scrapy_splash import SplashRequest
 
 from cloth.locators import Locators
 
+gender_type = {
+    'Women Home': 'Female',
+    'Men Home': 'Male',
+    'Kids Home': 'Kids'
+}
+
 
 class FarfetchSpider(scrapy.Spider):
     name = "cloth"
     url = ["https://www.farfetch.com/ca/shopping/women/dresses-1/items.aspx"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.items = []
 
     def start_requests(self):
         for url in self.url:
@@ -26,7 +37,7 @@ class FarfetchSpider(scrapy.Spider):
                                                                         'google_product_category': gpc})
 
     def parse_product(self, response):
-        yield {
+        item = {
             'id': response.xpath(Locators.ID).get().strip(),
             'item_group_id': response.xpath(Locators.ITEM_GROUP_ID).get().strip(),
             'mpn': response.xpath(Locators.MPN).get().strip(),
@@ -35,7 +46,7 @@ class FarfetchSpider(scrapy.Spider):
             'image_link': response.xpath(Locators.IMAGE_LINK).get(),
             'additional_image_link': response.xpath(Locators.ADDITIONAL_IMAGE_LINK).get(),
             'link': response.meta['product_link'],
-            'gender': 'women',
+            'gender': gender_type[response.xpath(Locators.CLOTH_GENDER_TYPE).get()],
             'availability': self.check_availability(response.xpath(Locators.AVAILABILITY).get()),
             'brand': response.xpath(Locators.BRAND).get(),
             'price': response.xpath(Locators.PRICE).get(),
@@ -44,9 +55,23 @@ class FarfetchSpider(scrapy.Spider):
                                        + response.meta['google_product_category'],
         }
 
+        self.items.append(item)
+        return item
+
     @staticmethod
     def check_availability(availability):
         if availability:
             return availability
         else:
             return 'In stock'
+
+    def closed(self, reason):
+        root = ET.Element("items")
+        for item_data in self.items:
+            xml_item = ET.SubElement(root, "item")
+            for key, value in item_data.items():
+                child = ET.SubElement(xml_item, key)
+                child.text = value if value else ''
+
+        tree = ET.ElementTree(root)
+        tree.write("items.xml", xml_declaration=True)
